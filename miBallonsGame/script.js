1db64c4f6158ae4a12155150011e4c6300b284fb
xTomyxx <tomvelasquezva@gmail.com>
2023-07-21 22:14:45 -0400

    21/07/2023

--- miBallonsGame/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("yellowBalloon").addEventListener('mousedown', startGame);
-
-
-function startGame() {
-    document.getElementById("yellowBalloon").style.visibility = "hidden";
-}
