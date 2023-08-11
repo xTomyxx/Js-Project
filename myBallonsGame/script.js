1481642c3a175d1a19d50d5c80025d31eb8b5167
Daniel <dny.velasquez@gmail.com>
2023-08-10 22:39:58 -0500

    grid y futbol

--- myBallonsGame/script.js
+++ myBallonsGame/script.js
@@ -1,14 +1,47 @@
-let balloonImg = document.getElementById("yellowBalloon")
 let score = 0;
-balloonImg.addEventListener('mousedown', startGame);
-document.getElementById("restart").addEventListener('mousedown', restarGame);
+let footBall = 0;
+let basketBall = 0;
+//let balloonImg = document.querySelector(".yellowBalloon");
+let scoreBoard = document.getElementById("scoreBoard");
+let message = document.getElementById("ballsGalery");
+//let restart = document.getElementById("restart");
 
-function startGame() {
-    balloonImg.style.visibility = "hidden";
-    score = score + 1; 
-    scoreBoard.textContent = score;
+document.addEventListener('mousedown', startGame);
+
+function startGame(pEvent) {
+    if(pEvent.target.className == "footBall"){
+        pEvent.target.style.visibility = "hidden";
+        score = score + 1; 
+        scoreBoard.textContent = score;
+        footBall = footBall + 1;
+        endGame();
+    }else if (pEvent.target.className == "basketBall"){
+        pEvent.target.style.visibility = "hidden";
+        score = score - 1; 
+        scoreBoard.textContent = score;
+        basketBall = basketBall + 1;
+        alert("You should click on the Football balls")
+        endGame()
+
+    }else if (pEvent.target.className == "restartBtn"){
+        restartGame();
+    
+    } else {
+        alert("You should click on the Football balls");
+    }
 }
 
-function restart() {
-    balloonImg.style.visibility = "visible";
+function endGame(){
+    if (footBall==6){
+        message.textContent = "Well done!! You won"
+        message.style.backgroundColor = "green";
+    } 
+    if (basketBall==1){
+        message.textContent = "You choose a Basketball ball! Please, try again"
+        message.style.backgroundColor = "red";
+    } 
+}
+
+function restartGame() {
+    window.location.reload();
 }
